9cf1997f68374c07fc1765dd2a90952baee99bbc
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-08-08 11:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const statusPara = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+
+enterButton.addEventListener("click", function () {
+	statusPara.outerHtml = "<h1 id="status">Entered Metaverse</h1>";
+	
+})
